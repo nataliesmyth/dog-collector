a45a36530880d07aef3c12c8fc71c07e29f38b4d
Nataliesmyth <nataliesmyth11@gmail.com>
2021-05-25 00:47:28 -0400

    feedings on admin page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # import models here
-from .models import Dog
+from .models import Dog, Feeding
 
 # Register your models here.
 admin.site.register(Dog)
+admin.site.register(Feeding)
