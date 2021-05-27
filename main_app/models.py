de63662b33156a7a3c80e32036d876799ea6367f
Nataliesmyth <nataliesmyth11@gmail.com>
2021-05-27 16:52:00 -0400

    one-to-many relationship between user and cat

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
-
+from django.contrib.auth.models import User
 
 # A tuple of 2-tuples
 # The first item in each 2-tuple represents the value that will be stored in the database
@@ -27,6 +27,7 @@ class Dog(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
     toys = models.ManyToManyField(Toy)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
