eca1d1d962db63b2851a22ce15e3067887a69f86
Nataliesmyth <nataliesmyth11@gmail.com>
2021-05-27 12:13:17 -0400

    path for dog CRUD

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,8 +8,11 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('dogs/<int:dog_id>/add_photo/', views.add_photo, name='add_photo'),
     # Dog Routes
-    path('dogs', views.dogs_index, name='index'),
+    path('dogs/', views.dogs_index, name='index'),
     path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
+    path('dogs/<int:dog_id>/edit/', views.edit_dog, name='edit_dog'),
+    path('dogs/<int:dog_id>/delete', views.delete_dog, name='delete_dog'),
+    path('dogs/new', views.add_dog, name="add_dog")
     # Feeding Routes
     path('dogs/<int:dog_id>/add_feeding/', views.add_feeding, name='add_feeding'),
     # Toy Routes: associate a toy with a dog (M:M)
