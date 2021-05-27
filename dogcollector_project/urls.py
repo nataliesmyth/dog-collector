282b4bef1ccf3dc1efd092c39bb9046d6ec6fbd7
Nataliesmyth <nataliesmyth11@gmail.com>
2021-05-27 17:01:37 -0400

    users can log in successfully

--- dogcollector_project/urls.py
+++ dogcollector_project/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # In this case, '' represents the root route
     path('', include('main_app.urls')),
+    # Include built in auth urls for built in views
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
